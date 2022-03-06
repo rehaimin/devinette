b16d75900cc01581357d66ed81b935d01ca777bb
mohammedrehaimin <mrehaimin@gmail.com>
2022-03-06 18:12:43 +0100

    Refactoring JS Variables

--- js/index.js
+++ js/index.js
@@ -6,56 +6,67 @@ function nameIsAvailable() {
         return true;
     }
 }
+const nameContainer = document.getElementById("nameContainer");
+const game = document.getElementById("game");
+const gamerNameSpan = document.getElementById("gamerName");
 
 function askForNameIfNotAvalable() {
     if (!nameIsAvailable()) {
-        document.getElementById("nameContainer").style.display = "block";
+        nameContainer.style.display = "block";
     } else {
-        document.getElementById("gamerName").innerText = gamerName;
-        document.getElementById("game").style.display = "block";
+        gamerNameSpan.innerText = gamerName;
+        game.style.display = "block";
     }
 
 }
+
+
+const name = document.getElementById("name");
 
 function verfyGivenName() {
-    if (document.getElementById("name").value != "") {
-        localStorage.setItem('gamerName', document.getElementById("name").value);
-        document.getElementById("nameContainer").style.display = "none";
-        document.getElementById("gamerName").innerText = localStorage.getItem('gamerName');
-        document.getElementById("game").style.display = "block";
+    if (name.value != "") {
+        localStorage.setItem('gamerName', name.value);
+        nameContainer.style.display = "none";
+        gamerNameSpan.innerText = localStorage.getItem('gamerName');
+        game.style.display = "block";
     }
 }
+
+const gamerScoreSpan = document.getElementById("gamerScore");
 
 function checkGamerScore() {
     if (localStorage.getItem('gamerScore') == null) {
         localStorage.setItem('gamerScore', 0);
-        document.getElementById("gamerScore").innerText = 0;
+        gamerScoreSpan.innerText = 0;
     } else {
-        document.getElementById("gamerScore").innerText = localStorage.getItem('gamerScore');
+        gamerScoreSpan.innerText = localStorage.getItem('gamerScore');
     }
 }
 
 let randomNumber;
 let leftTrials;
 let levelPoints;
+const mainMessage = document.getElementById("mainMessage");
+const trialsMessage = document.getElementById("trialsMessage");
+const pointsMessage = document.getElementById("pointsMessage");
 
 function easyLevel() {
     leftTrials = 3;
     levelPoints = 1;
-    document.getElementById("message").style.color = "#CCFF00";
-    document.getElementById("message").innerText = "Entrez un nombre entier entre 1 et 10";
-    document.getElementById("trialsMessage").innerText = "Vous avez " + leftTrials + " Essais pour deviner";
-    document.getElementById("pointsMessage").innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " point";
+    mainMessage.style.color = "#CCFF00";
+    mainMessage.innerText = "Entrez un nombre entier entre 1 et 10";
+    trialsMessage.innerText = "Vous avez " + leftTrials + " Essais pour deviner";
+    pointsMessage.innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " point";
     randomNumber = Math.ceil(Math.random() * 10);
 }
 
 function mediumLevel() {
     leftTrials = 5;
     levelPoints = 3;
-    document.getElementById("message").style.color = "#CCFF00";
-    document.getElementById("message").innerText = "Entrez un nombre entier entre 10 et 100";
-    document.getElementById("trialsMessage").innerText = "Vous avez " + leftTrials + " Essais pour deviner";
-    document.getElementById("pointsMessage").innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " points";
+    mainMessage.style.color = "#CCFF00";
+    mainMessage.innerText = "Entrez un nombre entier entre 10 et 100";
+    trialsMessage.innerText = "Vous avez " + leftTrials + " Essais pour deviner";
+    pointsMessage.innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " points";
     randomNumber = Math.ceil(Math.random() * 100);
     if (randomNumber < 10) {
         randomNumber = randomNumber + 10;
@@ -65,10 +76,10 @@ function mediumLevel() {
 function difficultLevel() {
     leftTrials = 10;
     levelPoints = 5;
-    document.getElementById("message").style.color = "#CCFF00";
-    document.getElementById("message").innerText = "Entrez un nombre entier entre 100 et 1000";
-    document.getElementById("trialsMessage").innerText = "Vous avez " + leftTrials + " Essais pour deviner";
-    document.getElementById("pointsMessage").innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " points";
+    mainMessage.style.color = "#CCFF00";
+    mainMessage.innerText = "Entrez un nombre entier entre 100 et 1000";
+    trialsMessage.innerText = "Vous avez " + leftTrials + " Essais pour deviner";
+    pointsMessage.innerText = "Pour chaque nombre valide vous gagnez " + levelPoints + " points";
     randomNumber = Math.ceil(Math.random() * 1000);
     if (randomNumber < 100) {
         randomNumber = randomNumber + 100;
@@ -76,23 +87,25 @@ function difficultLevel() {
 }
 
 let checkedRadio;
+const radioButtons = document.getElementsByName("levels");
+const givenNumberInput = document.getElementById("givenNumber");
 
 function isAnyLevelChecked(levelChecked) {
     for (let i = 0; i < 3; i++) {
-        let radioButton = document.getElementsByName("levels")[i];
+        let radioButton = radioButtons[i];
         if (radioButton.checked) {
             levelChecked = true;
         }
     }
     if (levelChecked != true) {
-        document.getElementById("message").style.color = "red";
-        document.getElementById("givenNumber").blur();
+        mainMessage.style.color = "red";
+        givenNumberInput.blur();
     }
 }
 
 function uncheckRadioButtons() {
     for (let j = 0; j < 3; j++) {
-        let radioButton = document.getElementsByName("levels")[j];
+        let radioButton = radioButtons[j];
         radioButton.checked = false;
         checkedRadio == undefined;
     }
@@ -100,35 +113,37 @@ function uncheckRadioButtons() {
 
 function disableRadioButtons() {
     for (let k = 0; k < 3; k++) {
-        document.getElementsByName("levels")[k].disabled = true;
+        radioButtons[k].disabled = true;
     }
 }
 
+const gameResultMessage = document.getElementById("gameResultMessage");
+const playAgainMessage = document.getElementById("playAgain");
 
 function verfyGivenNumber() {
-    let givenNumber = document.getElementById("givenNumber");
+    let givenNumber = givenNumberInput;
     let sPoints;
     let sTrials;
     (levelPoints > 1) ? sPoints = "s": sPoints = "";
     if (givenNumber.value != "") {
         if (givenNumber.value == randomNumber) {
             localStorage.setItem('gamerScore', parseInt(localStorage.getItem('gamerScore')) + levelPoints)
-            document.getElementById("gameResultMessage").style.color = "green";
-            document.getElementById("gameResultMessage").innerText = "Bravo vous avez gagné " + levelPoints + " point" + sPoints;
-            document.getElementById("playAgain").innerText = "Voulez vous continuer?"
+            gameResultMessage.style.color = "green";
+            gameResultMessage.innerText = "Bravo vous avez gagné " + levelPoints + " point" + sPoints;
+            playAgainMessage.innerText = "Voulez vous continuer?"
             gameOver();
         } else {
-            document.getElementById("message").style.color = "red";
-            document.getElementById("message").innerText = "Désolé ce n'est pas le bon nombre!";
+            mainMessage.style.color = "red";
+            mainMessage.innerText = "Désolé ce n'est pas le bon nombre!";
             leftTrials = leftTrials - 1;
             (leftTrials > 1) ? sTrials = "s": sTrials = "";
-            document.getElementById("pointsMessage").innerText = ""
-            document.getElementById("trialsMessage").innerText = "Il vous reste " + leftTrials + " Essai" + sTrials;
+            pointsMessage.innerText = ""
+            trialsMessage.innerText = "Il vous reste " + leftTrials + " Essai" + sTrials;
             disableRadioButtons();
             if (leftTrials == 0) {
-                document.getElementById("gameResultMessage").style.color = "red";
-                document.getElementById("gameResultMessage").innerText = "Désolé vous avez perdu!"
-                document.getElementById("playAgain").innerText = "Voulez vous réessayer?"
+                gameResultMessage.style.color = "red";
+                gameResultMessage.innerText = "Désolé vous avez perdu!"
+                playAgainMessage.innerText = "Voulez vous réessayer?"
                 gameOver();
             }
         }
@@ -137,20 +152,25 @@ function verfyGivenNumber() {
 
 function clearGivenNumber() {
     if (givenNumber.value != "") {
-        document.getElementById("givenNumber").value = "";
+        givenNumberInput.value = "";
     }
 }
 
+const gameResult = document.getElementById("gameResult");
+const goodByeName = document.getElementById("goodByeName");
+const goodByeScore = document.getElementById("goodByeScore");
+const goodByeMessage = document.getElementById("goodBye");
+
 function gameOver() {
-    document.getElementById("game").style.display = "none";
-    document.getElementById("gameResult").style.display = "block";
+    game.style.display = "none";
+    gameResult.style.display = "block";
 }
 
 function goodBye() {
-    document.getElementById("gameResult").style.display = "none";
-    document.getElementById("goodByeName").innerText = localStorage.getItem('gamerName') + " !";
-    document.getElementById("goodByeScore").innerText = localStorage.getItem('gamerScore');
-    document.getElementById("goodBye").style.display = "block";
+    gameResult.style.display = "none";
+    goodByeName.innerText = localStorage.getItem('gamerName') + " !";
+    goodByeScore.innerText = localStorage.getItem('gamerScore');
+    goodByeMessage.style.display = "block";
 }
 
 function playAgain() {
